6b5bec9367c3812d4ff1144a7a81cba65a78c8a0
Jason <jsb9886@rit.edu>
2023-02-21 18:11:18 -0500

    stubbed files for csv reader

---
+++ src/nutriapp/Ingredient.java
@@ -0,0 +1,13 @@
+package nutriapp;
+
+public class Ingredient {
+    String name;
+
+    /*
+     * Stub
+     * Will contain many other attributes, feel free to add them
+     */
+    public Ingredient(String name) {
+        this.name = name;
+    }
+}
---
+++ src/persistance/CsvReader.java
@@ -0,0 +1,14 @@
+package persistance;
+
+import nutriapp.*;
+
+public class CsvReader
+{
+    /*
+     * Stub
+     * Returns array of ingredients from filepath csv file
+     */
+    public Ingredient[] readIngredients(String filepath) {
+        return null;
+    }
+}
