7a3bbeb5242b744614c65619757c9efae73bd21d
Collin Cleary <cjc7891@rit.edu>
2023-02-23 09:38:11 -0500

    first iteration readIngredients and Ingredient

--- src/nutriapp/Ingredient.java
+++ src/nutriapp/Ingredient.java
@@ -2,12 +2,20 @@ package nutriapp;
 
 public class Ingredient {
     String name;
+    int num;
+    double protein;
+    double carbs;
+    int calories;
 
     /*
      * Stub
      * Will contain many other attributes, feel free to add them
      */
-    public Ingredient(String name) {
+    public Ingredient(String name, int num, double protein, double carbs, int calories) {
         this.name = name;
+        this.num = num;
+        this.protein = protein;
+        this.carbs = carbs;
+        this.calories = calories;
     }
 }
