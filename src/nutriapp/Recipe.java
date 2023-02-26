b56353281ec3bc7523479cc6a8c81db75a215126
nsp4746 <nsp4746@rit.edu>
2023-02-26 13:10:04 -0500

    Recipe

---
+++ src/nutriapp/Recipe.java
@@ -0,0 +1,56 @@
+package nutriapp;
+
+import java.util.ArrayList;
+
+public class Recipe extends Food {
+    private ArrayList<Ingredient> ingredients;
+    
+    public Recipe(double calories, double protein, double carbs, String name, int id, ArrayList<Ingredient> ingredients) {
+        super(calories, protein, carbs, name, id);
+        this.ingredients = ingredients;
+    }
+
+    public boolean addIngredient(Ingredient i) {
+        int size = ingredients.size();
+        int newSize = size + 1;
+        ingredients.add(i);
+        if (ingredients.size() == newSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeIngredient(Ingredient i){
+        int size = ingredients.size();
+        int newSize = size - 1;
+        ingredients.remove(i);
+        if (ingredients.size() == newSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Getters
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    /**
+     * Simple function that uses a for each loop to assemble a string that only has the names of the ingredients
+     * @return String of all ingredients in the recipe
+     */
+    public String ingredientsString(){
+        String s = "";
+        for(Ingredient i : this.ingredients){
+            s += i.getName() + ", ";
+        }
+        return s;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe=" + super.toString() + ", ingredients=" + ingredientsString();
+    }
+}
