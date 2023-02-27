4a0ccc9a6ca09fff8432d688d405a9887eb2ca60
ennuo <ajr.uiz2048@gmail.com>
2023-02-27 00:16:21 -0500

    Command line main menu

--- nutriapp/src/main/java/com/group3/nutriapp/App.java
+++ nutriapp/src/main/java/com/group3/nutriapp/App.java
@@ -1,13 +1,16 @@
 package com.group3.nutriapp;
 
-/**
- * Hello world!
- *
- */
+import com.group3.nutriapp.persistence.FoodFileDAO;
+import com.group3.nutriapp.persistence.HistoryFileDAO;
+import com.group3.nutriapp.persistence.UserFileDAO;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        new CLI(
+            new FoodFileDAO(),
+            new UserFileDAO(),
+            new HistoryFileDAO()
+        ).run();
     }
 }
