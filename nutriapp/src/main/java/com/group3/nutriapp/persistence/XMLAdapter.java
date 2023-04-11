03a158b5105f87b52401757c8be1054e0b1174f3
nsp4746 <nikhilpat67@gmail.com>
2023-04-10 21:22:21 -0400

    XML in progress

--- nutriapp/src/main/java/com/group3/nutriapp/persistence/XMLAdapter.java
+++ nutriapp/src/main/java/com/group3/nutriapp/persistence/XMLAdapter.java
@@ -1,5 +1,12 @@
 package com.group3.nutriapp.persistence;
 
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
 /**
  * @author Nikhil Patil + Group 3
  * @version 1.0
@@ -7,5 +14,22 @@ package com.group3.nutriapp.persistence;
  * @dateCreated 4/10/2023
  */
 public class XMLAdapter {
-    
+
+    private File file;
+
+    private final String FILEPATH = " ";
+
+    public XMLAdapter() {
+        this.file = new File(FILEPATH);
+    }
+
+    public void parseXML(){
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
