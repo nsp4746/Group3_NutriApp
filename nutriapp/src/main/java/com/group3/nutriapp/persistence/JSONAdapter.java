436b0cdbd460f78522d8f2955e481ba3c2052a1d
nsp4746 <nikhilpat67@gmail.com>
2023-04-10 20:45:21 -0400

    Jsonadapter

---
+++ nutriapp/src/main/java/com/group3/nutriapp/persistence/JSONAdapter.java
@@ -0,0 +1,73 @@
+package com.group3.nutriapp.persistence;
+
+import java.io.File;
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * @author Nikhil Patil + Group 3
+ * @version 1.0
+ * @description This class is used to read and write JSON files.
+ * @dateCreated 4/10/2023
+ */
+public class JSONAdapter {
+
+    // Intialize Variables
+    private File JsonFile;
+    private JSONParser parser;
+    private String key; // Key to search for in JSON file
+
+    private final String FILEPATH = "..\\Group3_NutriApp\\data\\ingredients.json";
+
+    public JSONAdapter(String key) {
+        this.JsonFile = new File(FILEPATH);
+        this.parser = new JSONParser();
+        this.key = key;
+    }
+
+    // Getters and Setters
+    public File getJsonFile() {
+        return JsonFile;
+    }
+
+    public void setJsonFile(File jsonFile) {
+        JsonFile = jsonFile;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * Method to parse a JSON file and return all the values when given a key. 
+     * {@link} https://www.geeksforgeeks.org/parse-json-java/
+     * @return empty string for now. Will most likely be changed
+     */
+    public String parseJSON() {
+        try {
+            Object obj = parser.parse(new FileReader(JsonFile));
+            JSONArray jsonObject = (JSONArray) obj;
+            
+            for (int i = 0; i < jsonObject.size(); i++) {
+                JSONObject ingredient = (JSONObject) jsonObject.get(i);
+                System.out.println(ingredient.get(key));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        JSONAdapter adapter = new JSONAdapter("name");
+        adapter.parseJSON();
+    }
+}
---
+++ nutriapp/src/main/java/com/group3/nutriapp/persistence/XMLAdapter.java
@@ -0,0 +1,11 @@
+package com.group3.nutriapp.persistence;
+
+/**
+ * @author Nikhil Patil + Group 3
+ * @version 1.0
+ * @description This class is used to read and write XML files.
+ * @dateCreated 4/10/2023
+ */
+public class XMLAdapter {
+    
+}
