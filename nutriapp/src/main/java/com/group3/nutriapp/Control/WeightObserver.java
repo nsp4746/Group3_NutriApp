15d1a4fbd41884bffd69bcf6e1a1d55de3e8c145
Logan Holmes <37guysinaroom@gmail.com>
2023-04-12 12:09:18 -0400

    added WeightObserver

---
+++ nutriapp/src/main/java/com/group3/nutriapp/Control/WeightObserver.java
@@ -0,0 +1,17 @@
+package com.group3.nutriapp.Control;
+
+import com.group3.nutriapp.persistence.UserFileDAO;
+
+public class WeightObserver implements Observer{
+   private UserFileDAO fileDAO;
+   private Goal goal;
+   private User user;
+   
+   public WeightObserver(Goal goal, User User){
+      
+   }
+
+   public void update(){
+      
+   }
+}
