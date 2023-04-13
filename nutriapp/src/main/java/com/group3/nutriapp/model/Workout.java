8a297f76624026e239384b0374771b5bdd2305e5
Logan Holmes <37guysinaroom@gmail.com>
2023-04-13 18:12:30 -0400

    fixed minutes intensity data types in Workout, added getCaloriesBurned

--- nutriapp/src/main/java/com/group3/nutriapp/model/Workout.java
+++ nutriapp/src/main/java/com/group3/nutriapp/model/Workout.java
@@ -4,13 +4,13 @@ import java.time.LocalDateTime;
 
 public class Workout {
    private int ID;
-   private double minutes;
-   private int intensity;
+   private int minutes;
+   private double intensity;
    private int sets;
    private int reps;
    private LocalDateTime date;
 
-   public Workout(int ID, double minutes, int intensity, int sets, int reps, LocalDateTime date){
+   public Workout(int ID, int minutes, double intensity, int sets, int reps, LocalDateTime date){
       this.ID = ID;
       this.minutes = minutes;
       this.intensity = intensity;
@@ -23,11 +23,15 @@ public class Workout {
       return ID;
    }
 
-   public double getMinutes() {
+   public double getCaloriesBurned(){
+      return minutes * intensity;
+   }
+
+   public int getMinutes() {
       return minutes;
    }
 
-   public int getIntensity() {
+   public double getIntensity() {
       return intensity;
    }
 
