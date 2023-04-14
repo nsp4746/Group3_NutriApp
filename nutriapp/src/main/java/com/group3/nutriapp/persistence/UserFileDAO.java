67e22e42bd2b2e3ab545a5d03466c92a518f2466
Logan Holmes <37guysinaroom@gmail.com>
2023-04-14 18:43:44 -0400

    added findUser() in UserFileDAO

--- nutriapp/src/main/java/com/group3/nutriapp/persistence/UserFileDAO.java
+++ nutriapp/src/main/java/com/group3/nutriapp/persistence/UserFileDAO.java
@@ -16,71 +16,86 @@ public class UserFileDAO {
     private int nextUserID;
     private ObjectMapper objectMapper;
 
-   private boolean save() {
-      User[] userArray = getUserArray();
-      try { objectMapper.writeValue(new File("data/users.json"), userArray); }
-      catch (Exception ex) { return false; }
-      return true;
-   }
+   
 
-   private boolean load() {
-      this.users = new HashMap<Integer, User>();
+    private boolean save() {
+        User[] userArray = getUserArray();
+        try { objectMapper.writeValue(new File("data/users.json"), userArray); }
+        catch (Exception ex) { return false; }
+        return true;
+    }
 
-      User[] users;
-      try { users = objectMapper.readValue(new File("data/users.json"), User[].class); }
-      catch (Exception ex) { return false; }
+    private boolean load() {
+        this.users = new HashMap<Integer, User>();
 
-      for (User user : users) {
-         int id = user.getId();
-         this.users.put(id, user);
-         if (id > this.nextUserID)
-            this.nextUserID = id;
-      }
+        User[] users;
+        try { users = objectMapper.readValue(new File("data/users.json"), User[].class); }
+        catch (Exception ex) { return false; }
 
-      this.nextUserID++;
+        for (User user : users) {
+            int id = user.getId();
+            this.users.put(id, user);
+            if (id > this.nextUserID)
+                this.nextUserID = id;
+        }
 
-      return true;
-   }
+        this.nextUserID++;
 
-   public User getUser(String username) {
-      for (User user : this.users.values()) {
-         if (user.getName().equals(username))
+        return true;
+    }
+
+    public User getUser(String username) {
+        for (User user : this.users.values()) {
+            if (user.getName().equals(username))
+                return user;
+        }
+        return null;
+    }
+
+    public User[] getUserArray(){
+        return findUser(null);
+    }
+
+    public User[] findUser(String containsText){
+        ArrayList<User> userList = new ArrayList<>();
+        for (User user : this.users.values()) {
+            if(containsText == null || user.getName().contains(containsText)) {
+                userList.add(user);
+            }
+        }
+
+        User[] userArray = new User[userList.size()];
+        userList.toArray(userArray);
+        return userArray;
+    }
+
+    public User addUser(String name, double height, double weight, LocalDate birthday, String password){
+        int age = (int) ChronoUnit.YEARS.between(birthday, LocalDate.now());
+        User user = new User(nextUserID, name, height, weight, age, password);
+        users.put(user.getId(), user);
+        save();
+        nextUserID++;
+        return user;
+    }
+
+    public User updateUser(User user){
+        if(!users.containsKey(user.getId())){
+            return null;
+        }
+        else{
+            users.put(user.getId(), user);
+            save();
             return user;
-      }
-      return null;
-   }
+        }
+    }
 
-   public User[] getUserArray(){
-      return findUsers(null);
-   }
-
-   public User addUser(String name, double height, double weight, LocalDate birthday, String password){
-      int age = (int) ChronoUnit.YEARS.between(birthday, LocalDate.now());
-      User user = new User(nextUserID, name, height, weight, age, password);
-      users.put(user.getId(), user);
-      save();
-      nextUserID++;
-      return user;
-   }
-
-   public User updateUser(User user){
-      if(!users.containsKey(user.getId())){
-         return null;
-      }
-      else{
-         users.put(user.getId(), user);
-         save();
-         return user;
-      }
-   }
-
-   public boolean deleteUser(int ID){
-      if(users.containsKey(ID)){
-         users.remove(ID);
-         return save();
-      }
-      else {
-         return false;
-      }
-   }
+    public boolean deleteUser(int ID){
+        if(users.containsKey(ID)){
+            users.remove(ID);
+            return save();
+        }
+        else {
+            return false;
+        }
+    }
 }
